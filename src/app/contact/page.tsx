d8ac645ee7a35cab9576edb5a8793717da2538be
dcalenzani <dcalenzani@pucp.pe>
2023-07-07 15:35:23 -0500

    fixed error in contact.page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,6 @@
-import NavBar from "@component/components/NavBar";
-
 export default function Contact() {
     return (
       <div>
-        <NavBar/>
         <h1>Contact</h1>
         <p>This is the contact page</p>
       </div>
