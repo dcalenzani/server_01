9596af12aa1e9c2eca437282d8ae9aecf5aa50ff
CarlosJose <carlosjose@HUARANGA.huaranga.net>
2024-02-16 11:12:47 -0500

    last commit didnt work

--- src/app/components/PostPreview.tsx
+++ src/app/components/PostPreview.tsx
@@ -1,10 +1,12 @@
 import { PostMetadata } from "./PostMetadata";
 
 const PostPreview = (props: PostMetadata) => {
+  const dateString = new Date(props.date).toLocaleDateString();
+
   return (
     <a className="group rounded-lg border border-neutral-900 space-y-2 my-2 mx-1 px-5 py-2 transition-colors h-[200px] hover:border-neutral-700 hover:bg-neutral-800/30" href={`/projects/${props.slug}`}>
       <h2 className="font-bold">{props.title}</h2>
-      <p className="text-sm text-red-500 dark:text-yellow-300">{props.date}</p>
+      <p className="text-sm text-red-500 dark:text-yellow-300">{dateString}</p>
       <p className="italic">{props.excerpt}</p>
     </a>
   );
